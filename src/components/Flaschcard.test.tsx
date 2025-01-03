e87e0671e2958f459b24039b316d66267e03d173
The Fox <lisieckikamil93@gmail.com>
2025-01-03 09:42:51 +0100

    feat (flashcard): add test for editing flashcard

--- src/components/Flaschcard.test.tsx
+++ src/components/Flaschcard.test.tsx
@@ -3,17 +3,59 @@ import Flashcard from "./Flashcard";
 import { vi } from "vitest";
 
 describe("Flashcard", () => {
-  it("should render the front of the card", () => {
-    render(<Flashcard front="front" back="back" />);
-    expect(screen.getByText("front")).toBeInTheDocument();
+  it('user cannot save flashcard with empty "front" input', async () => {
+    const { getByTestId, getAllByRole } = render(
+      <Flashcard front="" back="" />
+    );
+    const editBtn = getAllByRole("button", { name: "Edit" });
+
+    fireEvent.click(editBtn[0]);
+    const input1 = (await getByTestId("input1")) as HTMLInputElement;
+    const saveBtn = getAllByRole("button", { name: "Save" });
+
+    expect(input1.value).toBe("");
+    expect(saveBtn[0]).toBeDisabled();
   });
-  it("should render the back of the card", () => {
-    render(<Flashcard front="front" back="back" />);
-    expect(screen.getByText("back")).toBeInTheDocument();
+
+  it('use can save data when "front" input is not empty', async () => {
+    const { getByTestId, getAllByRole } = render(
+      <Flashcard front="Front text" back="Back text" />
+    );
+    const editBtn = getAllByRole("button", { name: "Edit" });
+
+    fireEvent.click(editBtn[0]);
+    const input1 = getByTestId("input1") as HTMLInputElement;
+    const saveBtn = getAllByRole("button", { name: "Save" });
+
+    expect(input1.value).toBe("Front text");
+    expect(saveBtn[0]).toBeEnabled();
   });
-  it("should render the edit button", () => {
-    render(<Flashcard front="front" back="back" />);
-    const editButtons = screen.getAllByRole("button", { name: "Edit" });
-    expect(editButtons.length).toBeGreaterThanOrEqual(1);
+
+  it('user cannot save flashcard with empty "back" input', async () => {
+    const { getByTestId, getAllByRole } = render(
+      <Flashcard front="" back="" />
+    );
+    const editBtn = getAllByRole("button", { name: "Edit" });
+
+    fireEvent.click(editBtn[0]);
+    const input2 = (await getByTestId("input2")) as HTMLInputElement;
+    const saveBtn = getAllByRole("button", { name: "Save" });
+
+    expect(input2.value).toBe("");
+    expect(saveBtn[0]).toBeDisabled();
+  });
+
+  it('use can save data when "back" input is not empty', async () => {
+    const { getByTestId, getAllByRole } = render(
+      <Flashcard front="Front text" back="Back text" />
+    );
+    const editBtn = getAllByRole("button", { name: "Edit" });
+
+    fireEvent.click(editBtn[0]);
+    const input2 = getByTestId("input2") as HTMLInputElement;
+    const saveBtn = getAllByRole("button", { name: "Save" });
+
+    expect(input2.value).toBe("Back text");
+    expect(saveBtn[0]).toBeEnabled();
   });
 });
--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -43,8 +43,14 @@ const Flashcard: React.FC<FlashCard> = ({ _id, front, back }) => {
                 type="text"
                 onChange={(e) => setInput1(e.target.value)}
                 onClick={(e) => e.stopPropagation()}
+                data-testid="input1"
               />
-              <button onClick={() => handleEdit(_id)}>Save</button>
+              <button
+                disabled={input1.length === 0}
+                onClick={() => handleEdit(_id)}
+              >
+                Save
+              </button>
             </>
           ) : (
             <>
@@ -77,8 +83,14 @@ const Flashcard: React.FC<FlashCard> = ({ _id, front, back }) => {
                 type="text"
                 onChange={(e) => setInput2(e.target.value)}
                 onClick={(e) => e.stopPropagation()}
+                data-testid="input2"
               />
-              <button onClick={() => handleEdit(_id)}>Save</button>
+              <button
+                disabled={input2.length === 0}
+                onClick={() => handleEdit(_id)}
+              >
+                Save
+              </button>
             </>
           ) : (
             <>
